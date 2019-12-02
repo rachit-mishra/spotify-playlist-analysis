cd545252f9e34f97d245afecd9f86f7ab03a62c7
Rachit Mishra <rachitmishra@Rachits-MBP.attlocal.net>
2019-12-01 21:45:55 -0600

    Updated to run with spark submit

--- Files/features_processing.py
+++ Files/features_processing.py
@@ -6,53 +6,89 @@ import os
 from json.decoder import JSONDecodeError
 import json 
 import pandas as pd
+import json
 
-def authentication():
-    username = sys.argv[1]
-    client_id = '97b72e4133f3495c88c50016f4ac3347'
-    client_secret = '3bb3891a7a68420a817cdd9a5a2a5552'
-    redirect_uri = 'http://localhost:8888/callback'
-    scope = 'user-library-read'
+from pyspark import SparkContext, SparkFiles
+from pyspark.sql import SQLContext
+from pyspark.sql import HiveContext
+from pyspark import SparkConf
+
+"""
+Setting Spark Configurations
+"""
+################### Defining spark context/python conf ###############
+sc = SparkContext()
+sc.setLogLevel("WARN")
+sqlContext = HiveContext(sc)
+
+os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3'
+################### Defining spark context/python conf ###############
+
+
+
+
+def authentication(username):
+    conf_file_path = SparkFiles.get(sys.argv[3])
+    
+    with open(conf_file_path, 'r') as file:
+        conf_file = json.load(file)
+        #print(conf_file)
+    
+    
+    client_id = conf_file["access_credentials"]["client_id"]
+    client_secret = conf_file["access_credentials"]["client_secret"]
+    redirect_uri = conf_file["access_credentials"]["redirect_uri"]
+    scope = conf_file["access_credentials"]["scope"]
+    
     try:
         token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
     except (AttributeError, JSONDecodeError):
-        os.remove(f".cache-{username}")
+        #os.remove(f".cache-{username}")
         token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-    return username, token 
+    return token 
 
-def retrieve_playlist_content(playlist_id):
+def retrieve_playlist_content(username, playlist_id):
     """
     Retrieving the content from each playlist
     """
     playlist_ids = []
     song_content = []
     count = 0  
-    username, token = authentication()
+    token = authentication(username)
+    song_names = []
     if token:
         sp = spotipy.Spotify(auth=token)
         playlists = sp.user_playlists(username)
         playlist_content = sp.user_playlist_tracks(username, playlist_id, limit = 100, fields = None)
-
+        
+        """
+        Logic to return tracks in a playlist
+        """
+        # for tracks in playlist_content['items']:
+        #     count+=1
+        # print(count)
+            #print(tracks['track'])
+        #     song_names.append(tracks['track']['name'])
+        # print(song_names)
         song_content.append(playlist_content['items'])
         if playlist_content['next'] is not None:
             count+=1
         else:
             exit
-        with open('{}-{}'.format(username,playlist_id), 'w') as write_dumps:
-            json.dump(song_content, write_dumps)
+        
     else:
         print("Unable to retrieve token for: ", username)    
 
     return sp, song_content, playlist_content , playlist_ids
 
 
-"""
-Method to process the audio features 
-for songs in the playlist
-"""
-def features_processing(playlist_id):
-    sp, playlists,playlist_content,playlist_ids = retrieve_playlist_content(playlist_id)
-    username, token = authentication()
+def features_processing(username, playlist_id):
+    """
+    Method to process the audio features 
+    for songs in the playlist
+    """
+    sp, playlists,playlist_content,playlist_ids = retrieve_playlist_content(username, playlist_id)
+    token = authentication(username)
     playlist_ids = []   
     song_content = []
     count = 0
@@ -73,40 +109,30 @@ def features_processing(playlist_id):
             list_features += sp.audio_features(playlist_ids[i:i+1])
             i+=1
 
-        print(list_features)
+        #print(list_features)
 
     else:
         print("Unable to retrieve token for: ", username)    
     
     return list_features
 
-def write_to_file(playlist_id):
-    list_features = features_processing(playlist_id)
-
+def write_to_file(username, playlist_id):
+    list_features = features_processing(username,playlist_id)
     processed_features = []
     """
     Writing audio features to a dataframe
     """
     for features in list_features:
         processed_features.append([features['energy'],
-        features['liveness'],
-                              features['tempo'], features['speechiness'],
-                              features['acousticness'], features['instrumentalness'],
-                              features['time_signature'], features['danceability'],
-                              features['key'], features['duration_ms'],
-                              features['loudness'], features['valence'],
-                              features['mode'], features['type'],features['uri']])
+        features['liveness'],features['tempo'], features['speechiness'],features['acousticness'], features['instrumentalness'],features['time_signature'], features['danceability'], features['key'], features['duration_ms'],features['loudness'], features['valence'],features['mode'], features['type'],features['uri']])
     
-    df = pd.DataFrame(processed_features, columns = ['energy',
-                                                'liveness','tempo',
-                                                'speechiness','acousticness',
-                                            'instrumentalness', 'time_signature', 'danceability',
-                                            'key', 'duration_ms', 'loudness','valence', 'mode', 'type', 'uri'])
-    df.to_csv('{}-{}.csv'.format(username,playlist_id), index=False)
-
+    df = pd.DataFrame(processed_features, columns = ['energy','liveness','tempo','speechiness','acousticness','instrumentalness', 'time_signature', 'danceability','key', 'duration_ms', 'loudness','valence', 'mode', 'type', 'uri'])
+    #df.to_csv('{}_____{}.csv'.format(username,playlist_id), index=False)
+    print(df.head(5))
+    return df
 
 def main(username, playlist):
-    username, token = authentication()
+    token = authentication(username)
     sp = spotipy.Spotify(auth=token)
     playlists = sp.user_playlists(username)
 
@@ -116,9 +142,9 @@ def main(username, playlist):
                 format(check_playlist['name'].encode('utf8'),
                         check_playlist['tracks']['total'],
                         check_playlist['id']))
-            retrieve_playlist_content(check_playlist['id'])
-            features_processing(check_playlist['id'])
-            write_to_file(check_playlist['id'])
+            retrieve_playlist_content(username, check_playlist['id'])
+            features_processing(username, check_playlist['id'])
+            write_to_file(username, check_playlist['id'])
 
 if __name__ == '__main__':
     username = sys.argv[1]
