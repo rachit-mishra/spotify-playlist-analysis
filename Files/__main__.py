304f5eebf2c8fb5af3ee363dd716af199eb304d2
Rachit Mishra <rachitmishra@Rachits-MBP.attlocal.net>
2019-12-08 02:34:33 -0600

    Initializing drivers

---
+++ Files/__main__.py
@@ -0,0 +1,10 @@
+import sys
+from features_processing import * 
+
+
+def main(args = None):
+    """ This is the main routine """
+    features_processing.main()
+
+if __name__ == "__main__":
+    main()
