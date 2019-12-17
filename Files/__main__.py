733250c7fd674aad8c9eff0c95dd480d2ad2ec39
Rachit Mishra <rachitmishra@Rachits-MBP.attlocal.net>
2019-12-16 20:16:46 -0600

    Executing via main

--- Files/__main__.py
+++ Files/__main__.py
@@ -1,10 +1,16 @@
 import sys
 from features_processing import * 
+import features_processing as f
 
-
-def main(args = None):
+def main():
     """ This is the main routine """
-    features_processing.main()
+    f.main(username, playlist)
+    
 
 if __name__ == "__main__":
+    sc = SparkContext()
+    sc.setLogLevel("WARN")
+    sqlContext = HiveContext(sc)
+    username = sys.argv[1]
+    playlist = sys.argv[2]
     main()
